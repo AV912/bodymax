c8fbd012f1aab07880453e7f50506e2f357dd203
AV912 <88800484+AV912@users.noreply.github.com>
2024-12-04 20:37:17 -0500

    Add Firebase configuration files

---
+++ lib/firebase_options.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'Web configuration not available yet.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'Platform not supported',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    // Add your android configuration here from google-services.json
+    apiKey: 'your-api-key',
+    appId: 'your-app-id',
+    messagingSenderId: 'your-sender-id',
+    projectId: 'your-project-id',
+    storageBucket: 'your-bucket',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    // Add your iOS configuration here from GoogleService-Info.plist
+    apiKey: 'your-api-key',
+    appId: 'your-app-id',
+    messagingSenderId: 'your-sender-id',
+    projectId: 'your-project-id',
+    storageBucket: 'your-bucket',
+    iosClientId: 'your-client-id',
+    iosBundleId: 'com.example.bodymax',
+  );
+}
